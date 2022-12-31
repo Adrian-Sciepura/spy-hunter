fdfc71320ebf2b0901d01abe4516b0026a2294b5
Adrian Ściepura <adriansciepura@outlook.com>
2022-12-31 17:39:27 +0100

    Dodaj pliki projektów.

---
+++ spy-hunter/main.cpp
@@ -0,0 +1,59 @@
+﻿#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include "./SDL2-2.0.10/include/SDL.h"
+#include "./SDL2-2.0.10/include/SDL_main.h"
+
+#define SCREEN_WIDTH	640
+#define SCREEN_HEIGHT	480
+
+
+int main(int argc, char **argv) {
+	bool quit = false;
+	SDL_Event event;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+
+	if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		printf("SDL_Init error");
+		return 1;
+	}
+                              
+	window = SDL_CreateWindow("Spy Hunter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		SDL_Quit();
+		printf("SDL_Window error");
+		return 1;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr)
+	{
+		SDL_Quit();
+		printf("SDL_Renderer error");
+		return 1;
+	}
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_ShowCursor(SDL_DISABLE);
+
+	while(!quit) {
+		SDL_RenderPresent(renderer);
+		while(SDL_PollEvent(&event)) {
+			switch(event.type) {
+				case SDL_QUIT:
+					quit = 1;
+					break;
+				};
+			};
+		};
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+};
