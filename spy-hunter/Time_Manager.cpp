4f2d19033928c69ca4a69bbc3a22101dffd59e9c
Adrian Ściepura <adriansciepura@outlook.com>
2023-01-02 22:20:52 +0100

    Added Time_Manager class

---
+++ spy-hunter/Time_Manager.cpp
@@ -0,0 +1,32 @@
+#include "Time_Manager.h"
+
+Time_Manager* Time_Manager::instance = nullptr;
+
+Time_Manager* Time_Manager::get_instance()
+{
+	if (instance == nullptr)
+		instance = new Time_Manager();
+	return instance;
+}
+
+void Time_Manager::destroy_instance()
+{
+	if (instance == nullptr)
+		return;
+	delete instance;
+	instance = nullptr;
+}
+
+Time_Manager::Time_Manager()
+{
+	delta = 0.0f;
+	last_update = 0;
+	elapsed_ticks = 0;
+}
+
+void Time_Manager::update()
+{
+	elapsed_ticks = SDL_GetTicks() - last_update;
+	delta = elapsed_ticks * 0.001f;
+	last_update = SDL_GetTicks();
+}
---
+++ spy-hunter/Time_Manager.h
@@ -0,0 +1,26 @@
+#ifndef TIME_MANAGER_H
+#define TIME_MANAGER_H
+
+#include "./SDL2-2.0.10/include/SDL.h"
+#include "./SDL2-2.0.10/include/SDL_main.h"
+
+class Time_Manager
+{
+private:
+	static Time_Manager* instance;
+
+	float delta;
+	int last_update;
+	int elapsed_ticks;
+
+	Time_Manager();
+
+public:
+	static Time_Manager* get_instance();
+	static void destroy_instance();
+
+	void update();
+};
+
+#endif
+
