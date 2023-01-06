ee6f4fc89847113f796c82128f44f000d806de3a
Adrian Ściepura <adriansciepura@outlook.com>
2023-01-06 20:42:44 +0100

    Fixed mouse-related functions

--- spy-hunter/Input_Manager.cpp
+++ spy-hunter/Input_Manager.cpp
@@ -6,7 +6,7 @@ Input_Manager* Input_Manager::get_instance()
 {
 	if (instance == nullptr)
 		instance = new Input_Manager();
-
+	
 	return instance;
 }
 
@@ -19,12 +19,31 @@ void Input_Manager::destroy_instance()
 	instance = nullptr;
 }
 
-void Input_Manager::update()
+void Input_Manager::update_keyboard()
 {
 	keyboard_state = SDL_GetKeyboardState(NULL);
 }
 
 bool Input_Manager::key_pressed(SDL_Scancode key)
 {
-	return keyboard_state[key];
+	return (keyboard_state[key] != 0);
+}
+
+void Input_Manager::update_mouse()
+{
+	int temp_x;
+	int temp_y;
+	mouse_state = SDL_GetMouseState(&temp_x, &temp_y);
+	mouse_pos = { (float)temp_x/Helper::SCREEN_SCALE, (float)temp_y/Helper::SCREEN_SCALE };
+}
+
+bool Input_Manager::mouse_button_pressed()
+{
+	Uint32 mask = SDL_BUTTON_LMASK;
+	return (mouse_state & mask) != 0;
+}
+
+Vector2 Input_Manager::get_mouse_pos()
+{
+	return mouse_pos;
 }
--- spy-hunter/Input_Manager.h
+++ spy-hunter/Input_Manager.h
@@ -3,18 +3,27 @@
 
 #include "./SDL2-2.0.10/include/SDL.h"
 #include "./SDL2-2.0.10/include/SDL_main.h"
+#include "Vector2.h"
+#include "Helper.h"
 
 class Input_Manager
 {
 private:
 	static Input_Manager* instance;
 	const Uint8* keyboard_state;
-
+	Uint32 mouse_state;
+	Vector2 mouse_pos;
+	
 public:
 	static Input_Manager* get_instance();
 	static void destroy_instance();
-	void update();
+
+	void update_keyboard();
 	bool key_pressed(SDL_Scancode key);
+
+	void update_mouse();
+	bool mouse_button_pressed();
+	Vector2 get_mouse_pos();
 };
 
 #endif
