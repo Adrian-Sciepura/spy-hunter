f01500f7deca329eacd79b98bd7dd3fe17fedd51
Adrian Ściepura <adriansciepura@outlook.com>
2023-01-06 20:41:18 +0100

    Added new useful functions

--- spy-hunter/Helper.cpp
+++ spy-hunter/Helper.cpp
@@ -10,6 +10,31 @@ const int Helper::SCREEN_SCALE = 2;
 SDL_Renderer* Helper::renderer = NULL;
 
 
+SDL_Color Helper::color_creator(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+	SDL_Color color;
+	color.r = r;
+	color.g = g;
+	color.b = b;
+	color.a = a;
+	return color;
+}
+
+SDL_Rect Helper::rectangle_creator(int x, int y, int w, int h)
+{
+	SDL_Rect rectangle;
+	rectangle.x = x;
+	rectangle.y = y;
+	rectangle.w = w;
+	rectangle.h = h;
+	return rectangle;
+}
+
+const SDL_Color Helper::gray = Helper::color_creator(150, 149, 153);
+const SDL_Color Helper::light_gray = Helper::color_creator(204, 203, 205);
+const SDL_Color Helper::black = Helper::color_creator(0, 0, 0);
+const SDL_Color Helper::red = Helper::color_creator(255, 0, 0);
+
 
 SDL_Texture* Helper::load_texture(const Dynamic_String& file, SDL_Renderer* renderer)
 {
@@ -22,7 +47,6 @@ SDL_Surface* Helper::load_surface(const Dynamic_String& file, SDL_Renderer* rend
 {
 	return nullptr;
 }
-
 
 void Helper::render_texture(SDL_Texture* texture, SDL_Renderer* renderer, SDL_Rect destination, SDL_Rect* clip)
 {
@@ -42,4 +66,13 @@ void Helper::render_texture(SDL_Texture* texture, SDL_Renderer* renderer, int x,
 		SDL_QueryTexture(texture, NULL, NULL, &dst.w, &dst.h);
 	}
 	render_texture(texture, renderer, dst, clip);
+}
+
+void Helper::render_rectangle(SDL_Renderer* renderer, const SDL_Rect* rectangle, const SDL_Color& color, bool filled)
+{
+	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+	if (filled)
+		SDL_RenderFillRect(renderer, rectangle);
+	else
+		SDL_RenderDrawRect(renderer, rectangle);
 }
--- spy-hunter/Helper.h
+++ spy-hunter/Helper.h
@@ -13,10 +13,20 @@ public:
 	static const int SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_SCALE;
 	static SDL_Renderer* renderer;
 
+	static SDL_Color color_creator(Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255);
+	static SDL_Rect rectangle_creator(int x, int y, int w, int h);
+
+	static const SDL_Color gray;
+	static const SDL_Color light_gray;
+	static const SDL_Color black;
+	static const SDL_Color red;
+
+
 	static SDL_Texture* load_texture(const Dynamic_String& file, SDL_Renderer* renderer = Helper::renderer);
 	static SDL_Surface* load_surface(const Dynamic_String& file, SDL_Renderer* renderer = Helper::renderer);
 	static void render_texture(SDL_Texture* texture, SDL_Renderer* renderer, SDL_Rect destination, SDL_Rect* clip);
 	static void render_texture(SDL_Texture* texture, SDL_Renderer* renderer, int x, int y, SDL_Rect* clip);
+	static void render_rectangle(SDL_Renderer* renderer, const SDL_Rect* rectangle, const SDL_Color& color, bool filled = false);
 };
 
 #endif
