b68d27c89a6db643243c1e542ba7fc1b2ffc03f4
Adrian Ściepura <adriansciepura@outlook.com>
2023-01-09 00:19:59 +0100

    Added a rebuilt entity class

--- spy-hunter/Entity.cpp
+++ spy-hunter/Entity.cpp
@@ -1,108 +1,34 @@
 #include "Entity.h"
 
-Entity::Entity(SDL_Texture* texture, const Vector2& position, float max_speed)
+Entity::Entity(const Vector2& position, int width, int height, bool is_solid, float max_speed, SDL_Texture* texture) :
+	Object(position, width, height, is_solid, texture)
 {
-	this->position = position;
-	this->texture = texture;
-	this->collider = new Box_Collider({ 50, 80 }, { 0, 0 });
-	this->speed = 0;
 	this->max_speed = max_speed;
-	this->angle = 90;
+	this->speed = 0;
 	this->is_moving = false;
+	this->angle = 90.0f;
 }
 
 Entity::Entity(const Entity& entity) :
-	Entity(entity.texture, entity.position, entity.max_speed)
+	Entity(entity.position, entity.width, entity.height, entity.is_solid, entity.max_speed, entity.texture)
 {
 	
 }
 
-Entity::~Entity()
-{
-	delete collider;
-}
-
-void Entity::draw(const Vector2& camera_pos)
-{
-	Vector2 relaive_position 
-	{	this->position.x - camera_pos.x, 
-		this->position.y - camera_pos.y
-	};
-
-	Helper::render_texture(this->texture, Helper::renderer, relaive_position.x, relaive_position.y, nullptr);
-	this->collider->update(relaive_position);
-}
-
-void Entity::update_collisions()
-{
-	//int how_many_colliders = 0;
-	//for (int i = 0; i < number_of_entities; i++)
-	//{
-	//	if (entities[i] == this)
-	//		continue;
-
-	//	if (this->collider->check_collision(*(entities[i]->collider)))
-	//		how_many_colliders++;
-	//}
-
-	//if (how_many_colliders > 0)
-	//	this->collider->is_colliding = true;
-	//else
-	//	this->collider->is_colliding = false;
-}
-
-
-
 void Entity::update_movement()
 {
-	Input_Manager* input_manager = Input_Manager::get_instance();
-
-	if(!input_manager->key_pressed(SDL_SCANCODE_UP) && speed == 0)
-		is_moving = false;
-
-	if (input_manager->key_pressed(SDL_SCANCODE_UP))
-	{
-		is_moving = true;
-		if (input_manager->key_pressed(SDL_SCANCODE_LEFT))
-			angle = 135;
-		else if (input_manager->key_pressed(SDL_SCANCODE_RIGHT))
-			angle = 45;
-		else
-			angle = 90;
-	}
-	else if (input_manager->key_pressed(SDL_SCANCODE_DOWN))
-	{
-		angle = 270;
-	}
-	else if (is_moving)
-	{
-		if (input_manager->key_pressed(SDL_SCANCODE_LEFT))
-			angle = 135;
-		else if (input_manager->key_pressed(SDL_SCANCODE_RIGHT))
-			angle = 45;
-		else
-			angle = 90;
-	}
-
-
-	if(is_moving)
+	if (is_moving)
 	{
 		float move_dist = speed * Time_Manager::get_instance()->get_delta() * 4.0;
-		float x_move = move_dist * cos((angle * Helper::PI) / 180.0);
-		float y_move = move_dist * -sin((angle * Helper::PI) / 180.0);
+		float move_x = move_dist * cos(angle * Helper::PI / 180.0);
+		float move_y = move_dist * -sin(angle * Helper::PI / 180.0);
 
-		if (y_move > 0)
-		{
-			speed -= 1;
-			y_move = -y_move;
-		}
-		else
-		{
-			if ((speed + 1) <= max_speed)
-				speed += 1;
-		}
-		
-		position += {x_move, y_move};
+		position += {move_x, move_y};
 	}
+}
 
+void Entity::update()
+{
+	Object::update();
+	update_movement();
 }
--- spy-hunter/Entity.h
+++ spy-hunter/Entity.h
@@ -1,42 +1,22 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include "./SDL2-2.0.10/include/SDL.h"
-#include "./SDL2-2.0.10/include/SDL_main.h"
-#include "Vector2.h"
-#include "Helper.h"
-#include "Box_Collider.h"
-#include "Input_Manager.h"
+#include "Object.h"
 #include "Time_Manager.h"
 
-class Entity
+class Entity: public Object
 {
-private:
-	/*static Entity** entities;*/
-
 public:
-	//static int number_of_entities;
-	//static Entity** get_all_entities();
-	//static int get_number_of_entities();
-	//static void remove_all_entities();
-
-	Vector2 position;
-	SDL_Texture* texture;
-	Box_Collider* collider;
 	float speed;
 	float max_speed;
 	float angle;
 	bool is_moving;
 
-	Entity(SDL_Texture* texture, const Vector2& position, float max_speed);
+	Entity(const Vector2& position, int width, int height, bool is_solid, float max_speed, SDL_Texture* texture);
 	Entity(const Entity& entity);
-	~Entity();
-	void draw(const Vector2& camera_pos);
-	void update_collisions();
-	//void change_angle();
-	void update_movement();
-
+	
+	virtual void update_movement();
+	virtual void update() override;
 };
 
 #endif
-
