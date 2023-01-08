c1544fb5e1268706f9ddce3be705349b4fd95231
Adrian Ściepura <adriansciepura@outlook.com>
2023-01-08 20:44:32 +0100

    Added new drawing functions

--- spy-hunter/Helper.cpp
+++ spy-hunter/Helper.cpp
@@ -3,9 +3,12 @@
 const float Helper::PI = 3.1416;
 
 //4:3 ASPECT RATIO RESOLUTION
-const int Helper::SCREEN_WIDTH = 640;
-const int Helper::SCREEN_HEIGHT = 480;
-const int Helper::SCREEN_SCALE = 2;
+const int Helper::SCREEN_WIDTH = 640;	//850
+const int Helper::SCREEN_HEIGHT = 480;	//500
+const float Helper::SCREEN_SCALE = 1.5;
+
+const int Helper::TILES_X = SCREEN_WIDTH / 32;
+const int Helper::TILES_Y = SCREEN_HEIGHT / 32;
 
 SDL_Renderer* Helper::renderer = NULL;
 
@@ -40,6 +43,7 @@ SDL_Texture* Helper::load_texture(const Dynamic_String& file, SDL_Renderer* rend
 {
 	SDL_Surface* tmp = SDL_LoadBMP(file);
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, tmp);
+	
 	return texture;
 }
 
@@ -75,4 +79,49 @@ void Helper::render_rectangle(SDL_Renderer* renderer, const SDL_Rect* rectangle,
 		SDL_RenderFillRect(renderer, rectangle);
 	else
 		SDL_RenderDrawRect(renderer, rectangle);
+}
+
+void Helper::render_text(SDL_Surface* font, SDL_Surface* surface, const Dynamic_String& text, bool centered)
+{
+	const char* temp = text;
+	int x = 10;
+	int y = 10;
+	if (centered)
+	{
+		x = (surface->w - text.length())/4;
+		y = (surface->h - 8)/2;
+	}
+	int px, py, c;
+	SDL_Rect s, d;
+	s.w = 8;
+	s.h = 8;
+	d.w = 8;
+	d.h = 8;
+
+	while (*temp) 
+	{
+		c = *temp & 255;
+		px = (c % 16) * 8;
+		py = (c / 16) * 8;
+		s.x = px;
+		s.y = py;
+		d.x = x;
+		d.y = y;
+		SDL_BlitSurface(font, &s, surface, &d);
+		x += 8;
+		temp++;
+	};
+}
+
+void Helper::render_surface(SDL_Surface* surface, int x, int y)
+{
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	render_texture(texture, renderer, x, y, nullptr);
+	SDL_DestroyTexture(texture);
+}
+
+void Helper::draw_line(int x1, int y1, int x2, int y2, SDL_Color color)
+{
+	SDL_SetRenderDrawColor(renderer, color.a, color.g, color.b, color.a);
+	SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
 }
--- spy-hunter/Helper.h
+++ spy-hunter/Helper.h
@@ -10,7 +10,9 @@ class Helper
 {
 public:
 	static const float PI;
-	static const int SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_SCALE;
+	static const int SCREEN_WIDTH, SCREEN_HEIGHT;
+	static const float SCREEN_SCALE;
+	static const int TILES_X, TILES_Y;
 	static SDL_Renderer* renderer;
 
 	static SDL_Color color_creator(Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255);
@@ -21,13 +23,14 @@ public:
 	static const SDL_Color black;
 	static const SDL_Color red;
 
-
 	static SDL_Texture* load_texture(const Dynamic_String& file, SDL_Renderer* renderer = Helper::renderer);
 	static SDL_Surface* load_surface(const Dynamic_String& file, SDL_Renderer* renderer = Helper::renderer);
 	static void render_texture(SDL_Texture* texture, SDL_Renderer* renderer, SDL_Rect destination, SDL_Rect* clip);
 	static void render_texture(SDL_Texture* texture, SDL_Renderer* renderer, int x, int y, SDL_Rect* clip);
 	static void render_rectangle(SDL_Renderer* renderer, const SDL_Rect* rectangle, const SDL_Color& color, bool filled = false);
+	static void render_text(SDL_Surface* font, SDL_Surface* surface, const Dynamic_String& text, bool centered);
+	static void render_surface(SDL_Surface* surface, int x, int y);
+	static void draw_line(int x1, int y1, int x2, int y2, SDL_Color color = gray);
 };
 
 #endif
-
