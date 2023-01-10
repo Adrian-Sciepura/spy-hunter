894dfc057b79930dad319030c75780a45c1eeea8
Adrian Ściepura <adriansciepura@outlook.com>
2023-01-11 00:01:19 +0100

    Enlarge window in map editor mode

--- spy-hunter/main.cpp
+++ spy-hunter/main.cpp
@@ -5,47 +5,48 @@
 
 #include "./SDL2-2.0.10/include/SDL.h"
 #include "./SDL2-2.0.10/include/SDL_main.h"
+
 #include "Helper.h"
 #include "Game_Manager.h"
 #include "Map_Editor.h"
 
-
-int main(int argc, char** argv) 
+void init(SDL_Window*& window, int screen_width, int screen_height, float screen_scale)
 {
-	bool quit = false;
-	SDL_Event event;
-	SDL_Window *window;
-	
-	if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		printf("SDL_Init error");
-		return 1;
+		exit(1);
 	}
-                              
-	window = SDL_CreateWindow("Spy Hunter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
-		Helper::SCREEN_WIDTH * Helper::SCREEN_SCALE, Helper::SCREEN_HEIGHT * Helper::SCREEN_SCALE, SDL_WINDOW_SHOWN);
+
+	window = SDL_CreateWindow("Spy Hunter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		screen_width * screen_scale, screen_height * screen_scale, SDL_WINDOW_SHOWN);
 	if (window == nullptr)
 	{
 		SDL_Quit();
-		printf("SDL_Window error");
-		return 1;
+		printf("SDL_Window error"); 
+		exit(1);
 	}
 
 	Helper::renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (Helper::renderer == nullptr)
 	{
 		SDL_Quit();
-		printf("SDL_Renderer error");
-		return 1;
+		printf("SDL_Renderer error"); 
+		exit(1);
 	}
+	//SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+	SDL_RenderSetLogicalSize(Helper::renderer, screen_width, screen_height);
+}
 
-	SDL_RenderSetLogicalSize(Helper::renderer, Helper::SCREEN_WIDTH, Helper::SCREEN_HEIGHT);
-
+int main(int argc, char** argv) 
+{
+	SDL_Window *window;
 
 	if (argc > 1)
 	{
 		if (strcmp(argv[1], "-map_editor") == 0)
 		{
+			init(window, 850, 500, Helper::SCREEN_SCALE);
 			printf("MAP EDITOR \n");
 			Map_Editor* map_editor = Map_Editor::get_instance();
 			map_editor->run();
@@ -54,15 +55,16 @@ int main(int argc, char** argv)
 	}
 	else
 	{
+		init(window, Helper::SCREEN_WIDTH, Helper::SCREEN_HEIGHT, Helper::SCREEN_SCALE);
 		SDL_ShowCursor(SDL_DISABLE);
 		Game_Manager* game = Game_Manager::get_instance();
 		game->run();
-		game->destroy_instance();
+		//game->destroy_instance();
 	}
 
 	SDL_DestroyRenderer(Helper::renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
+	//_CrtDumpMemoryLeaks();
 	return 0;
 };
