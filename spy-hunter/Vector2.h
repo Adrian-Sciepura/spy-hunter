9248e24e978d17a70a8daddd79105cafcf52655d
Adrian Ściepura <adriansciepura@outlook.com>
2023-01-16 00:16:02 +0100

    deleted comments

--- spy-hunter/Vector2.h
+++ spy-hunter/Vector2.h
@@ -9,16 +9,12 @@ public:
 	Vector2(float x = 0, float y = 0);
 	Vector2(const Vector2& source);
 
-	//void translate(const Vector2& vector);
-
 	Vector2& operator = (const Vector2& source);
 	friend Vector2 operator + (const Vector2& first_element, const Vector2& second_element);
 	Vector2& operator += (const Vector2& vector);
-	//friend Vector2 operator + (const Vector2& first_element, int number);
 	friend Vector2 operator - (const Vector2& first_element, const Vector2& second_element);
 	Vector2& operator -= (const Vector2& vector);
 	friend bool operator == (const Vector2& first_vector, const Vector2& second_vector);
-	//friend Vector2 operator - (const Vector2& first_element, int number);
 };
 
 #endif
